574cc68da8e49dc1d6dc87c8ce8cfe695c0a12bd
Sameer Al Sahab <224245762+SameerAlSahab@users.noreply.github.com>
2026-01-17 01:07:08 +0600

    mods: overhaul nuke bloat

--- astro/nuke-bloat/Nuke-Bloat.sh
+++ astro/nuke-bloat/Nuke-Bloat.sh
@@ -1,59 +1,200 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 
 declare -a BLOAT_TARGETS=(
+    # Basic
+    BCService
+    CIDManager
+    DeviceKeystring
+    DiagMonAgent91
+    FacAtFunction
+    FactoryTestProvider
+    FotaAgent
+    KnoxGuard
+    ModemServiceMode
+    MyGalaxyService
+    PaymentFramework
+    Rampart
+    SEMFactoryApp
+    SHClient
+    SilentLog
+    SmartEpdgTestApp
+    SmartTutor
+    SmartTouchCall
+    SOAgent7
+    SsuService
+    Ts43AuthService
+    UnifiedTetheringProvision
+    UnifiedVVM
+    UsByod
+    WebManual
+    WlanTest
+    wssyncmldm
+    MapsAgent
+    AppUpdateCenter
+    LedCoverService
+    LiveTranscribe
+    IpsGeofence
+    VoiceAccess
+    SVoiceIME
 
-    # System Bloat
-    "AuthFramework" "BCService" "CIDManager" "DeviceKeystring" "DiagMonAgent91"
-    "DigitalKey" "FacAtFunction" "FactoryTestProvider" "FotaAgent" "KnoxGuard" "Rampart"
-    "ModemServiceMode" "PaymentFramework" "SEMFactoryApp" "SOAgent7" "SamsungCarKeyFw"
-    "SamsungPass" "SamsungPassAutofill_v1" "SilentLog" "SmartEpdgTestApp" "Ts43AuthService"
-    "UnifiedTetheringProvision" "UnifiedVVM" "UsByod" "WebManual" "WlanTest" "wssyncmldm" "MyGalaxyService"
-    "SsuService" "MapsAgent" "AppUpdateCenter" "LedCoverService" "LiveTranscribe"
-    "AREmojiEditor" "AvatarEmojiSticker" "SamsungCalendar" "MinusOnePage"
-    "OfflineLanguageModel_stub" "IpsGeofence" "SHClient" "SmartTouchCall"
-    "SmartTutor" "SVoiceIME" "VoiceAccess"
+    # Factory / Test
+    AutomationTest_FB
+    Cameralyzer
+    DRParser
+    FactoryAirCommandManager
+    FactoryCameraFB
+    HMT
 
-    # Factory/Test
-    "AutomationTest_FB" "DRParser" "FactoryCameraFB" "Cameralyzer" "FactoryAirCommandManager" "HMT"
-
-    # Facebook
-    "FBAppManager_NS" "FBInstaller_NS" "FBServices"
-
-    # TTS Voices
-    "SamsungTTSVoice_de_DE_f00" "SamsungTTSVoice_en_GB_f00" "SamsungTTSVoice_en_US_l03"
-    "SamsungTTSVoice_es_ES_f00" "SamsungTTSVoice_es_MX_f00" "SamsungTTSVoice_es_US_f00"
-    "SamsungTTSVoice_es_US_l01" "SamsungTTSVoice_fr_FR_f00" "SamsungTTSVoice_hi_IN_f00"
-    "SamsungTTSVoice_it_IT_f00" "SamsungTTSVoice_pl_PL_f00" "SamsungTTSVoice_pt_BR_f00"
-    "SamsungTTSVoice_pt_BR_l01" "SamsungTTSVoice_ru_RU_f00" "SamsungTTSVoice_th_TH_f00"
-    "SamsungTTSVoice_vi_VN_f00" "SamsungTTSVoice_id_ID_f00" "SamsungTTSVoice_ar_AE_m00"
+    # Meta
+    FBAppManager_NS
+    FBInstaller_NS
+    FBServices
 
     # Samsung Apps
-    "AssistantShell" "Notes40" "SBrowser" "SamsungPass" "SamsungPassAutofill_v1" "SamsungWallet"
+    AREmojiEditor
+    AvatarEmojiSticker
+    MinusOnePage
+    Notes40
+    Routines
+    SamsungCalendar
+    SBrowser
+
+    # Samsung TTS Voices
+    SamsungTTSVoice_ar_AE_m00
+    SamsungTTSVoice_de_DE_f00
+    SamsungTTSVoice_en_GB_f00
+    SamsungTTSVoice_en_US_l03
+    SamsungTTSVoice_es_ES_f00
+    SamsungTTSVoice_es_MX_f00
+    SamsungTTSVoice_es_US_f00
+    SamsungTTSVoice_es_US_l01
+    SamsungTTSVoice_fr_FR_f00
+    SamsungTTSVoice_hi_IN_f00
+    SamsungTTSVoice_id_ID_f00
+    SamsungTTSVoice_it_IT_f00
+    SamsungTTSVoice_pl_PL_f00
+    SamsungTTSVoice_pt_BR_f00
+    SamsungTTSVoice_pt_BR_l01
+    SamsungTTSVoice_ru_RU_f00
+    SamsungTTSVoice_th_TH_f00
+    SamsungTTSVoice_vi_VN_f00
 
     # Google Apps
-    "Chrome" "DuoStub" "Gmail2" "Maps" "Messages" "YouTube"
+    AssistantShell
+    Chrome
+    DuoStub
+    Gmail2
+    Maps
+    Messages
+    YouTube
+    PlayAutoInstallConfig
 
-    # Third-Party
-    "BlockchainBasicKit" "DictDiotekForSec" "HMT" "MoccaMobile" "OneDrive_Samsung_v3"
-    "PlayAutoInstallConfig" "Scone" "Upday" "VzCloud"
+    # Microsoft
+    OneDrive_Samsung_v3
 
-    # Game
-    "GameHome"
+    # Third-party
+    DictDiotekForSec
+    MoccaMobile
+    Scone
+    Upday
+    VzCloud
 
-    # Overlays
-    "GmsConfigOverlaySearchSelector.apk" "SearchSelector"
+    # Gaming
+    GameHome
+
+    # Extras
+    DsmsAPK
+    SearchSelector
+    GmsConfigOverlaySearchSelector.apk
+
+    # Data
+    DeviceQualityAgent36
+    SOAgent76
+    DiagMonAgent95
+
+    # Not gonna work
+    BlockchainBasicKit
+    SamsungCarKeyFw
+    SamsungPass
+    SamsungPassAutofill_v1
+    SamsungWallet
+    DigitalKey
+    AuthFramework
 )
-
-
-
 
 NUKE_BLOAT "${BLOAT_TARGETS[@]}"
 
+# Useless dirs
+declare -a SYSTEM_DIRS=(
+    hidden
+    preload
+)
 
-REMOVE "system" "hidden"
-REMOVE "system" "preload"
+for dir in "${SYSTEM_DIRS[@]}"; do
+    REMOVE "system" "$dir"
+done
 
 
 
+
+declare -a PERM_TO_REMOVE=(
+    # Digital Key / Wallet
+    permissions/org.carconnectivity.android.digitalkey.rangingintent.xml
+    permissions/org.carconnectivity.android.digitalkey.secureelement.xml
+    permissions/privapp-permissions-com.samsung.android.carkey.xml
+    permissions/privapp-permissions-com.samsung.android.dkey.xml
+    permissions/privapp-permissions-com.samsung.android.spayfw.xml
+    permissions/signature-permissions-com.samsung.android.spay.xml
+    permissions/signature-permissions-com.samsung.android.spayfw.xml
+    sysconfig/digitalkey.xml
+    sysconfig/preinstalled-packages-com.samsung.android.dkey.xml
+    sysconfig/preinstalled-packages-com.samsung.android.spayfw.xml
+
+    # Samsung Auth/ Pass
+    permissions/authfw.xml
+    permissions/privapp-permissions-com.samsung.android.authfw.xml
+    permissions/privapp-permissions-com.samsung.android.samsungpass.xml
+    permissions/signature-permissions-com.samsung.android.samsungpass.xml
+    permissions/signature-permissions-com.samsung.android.samsungpassautofill.xml
+    sysconfig/samsungauthframework.xml
+    sysconfig/samsungpassapp.xml
+
+    #  AR / Emoji
+    default-permissions/default-permissions-com.sec.android.mimage.avatarstickers.xml
+    permissions/privapp-permissions-com.samsung.android.aremojieditor.xml
+    permissions/privapp-permissions-com.sec.android.mimage.avatarstickers.xml
+    permissions/signature-permissions-com.sec.android.mimage.avatarstickers.xml
+
+    # OneDrive
+    permissions/privapp-permissions-com.microsoft.skydrive.xml
+
+    # Meta
+    default-permissions/default-permissions-meta.xml
+    permissions/privapp-permissions-meta.xml
+    sysconfig/meta-hiddenapi-package-allowlist.xml
+
+    # Update center and etc
+    sysconfig/feature-a11y-preload.xml
+    permissions/privapp-permissions-com.samsung.android.app.updatecenter.xml
+
+    # My Galaxy
+    permissions/privapp-permissions-com.mygalaxy.service.xml
+    sysconfig/preinstalled-packages-com.mygalaxy.service.xml
+
+    # Samsung Data gathering
+    permissions/privapp-permissions-com.samsung.android.dqagent.xml
+    permissions/privapp-permissions-com.sec.android.diagmonagent.xml
+    permissions/privapp-permissions-com.sec.android.soagent.xml
+
+    # Factory / Test
+    default-permissions/default-permissions-com.sec.factory.cameralyzer.xml
+    permissions/privapp-permissions-com.samsung.android.providers.factory.xml
+    permissions/privapp-permissions-com.sec.facatfunction.xml
+)
+
+for file in "${PERM_TO_REMOVE[@]}"; do
+    REMOVE "system" "etc/$file"
+done
 
 
