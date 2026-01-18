2c11d679020386a6e1a694d603e2653fceff6ce2
Sameer Al Sahab <224245762+SameerAlSahab@users.noreply.github.com>
2026-01-18 18:22:40 +0600

    installer: add warning text

--- astro/nuke-bloat/Nuke-Bloat.sh
+++ astro/nuke-bloat/Nuke-Bloat.sh
@@ -193,8 +193,8 @@ declare -a PERM_TO_REMOVE=(
     permissions/privapp-permissions-com.sec.facatfunction.xml
 )
 
-#LOG_INFO "Removing permisson files..."
+LOG_INFO "Removing permisson files..."
 
-#for file in "${PERM_TO_REMOVE[@]}"; do
+for file in "${PERM_TO_REMOVE[@]}"; do
     #SILENT REMOVE "system" "etc/$file"
-#done
+done
