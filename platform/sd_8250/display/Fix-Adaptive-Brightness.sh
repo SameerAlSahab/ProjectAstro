78ff6df87a847b2c05b9053e036b76a3b333144f
Sameer Al Sahab <224245762+SameerAlSahab@users.noreply.github.com>
2026-01-20 16:43:31 +0600

    objectives: x1q: init

---
+++ platform/sd_8250/display/Fix-Adaptive-Brightness.sh
@@ -0,0 +1,6 @@
+ADD_FROM_FW "dm3q" "vendor" "bin/hw/vendor.samsung.hardware.light-service"
+ADD_FROM_FW "dm3q" "vendor" "lib64/vendor.samsung.hardware.light-V1-ndk_platform.so"
+
+# Try to use from source if exist
+ADD_FROM_FW "main" "vendor" "lib64/vendor.samsung.hardware.light-V1-ndk_platform.so"
+
