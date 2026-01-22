8fc457f84265f15c94aaf4b7eeac670a9b7105db
Daubfy <149339273+Daubfy@users.noreply.github.com>
2026-01-22 15:10:27 -0500

    Fix ril
    fixes the ril by using one ui 7 ril library

--- objectives/r9s/vendor-patches/patches.sh
+++ objectives/r9s/vendor-patches/patches.sh
@@ -1,4 +1,5 @@
 #TEE have things of bootloader
-#We are building using latest firmware (latest binary and onr ui 8)
+#We are building using latest firmware (latest binary and one ui 8)
 # Latest unlockeable BL is based on binary I (one ui 7) so, we can use OneUI 8 vendor with vendor/tee folder from OneUI 7 (binary I)
+# Requires one ui 7 libsec-ril.so to have working ril
 REMOVE "vendor" "tee"
